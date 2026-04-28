6dd1a56393fbb022769a7f9540a205a7ba519849
AlanGao <alangao0924@gmail.com>
2026-04-28 10:16:00 +0800

    Initial commit

---
+++ src/test/java/com/dashuo/pcshop/PCshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashuo.pcshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PCshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
